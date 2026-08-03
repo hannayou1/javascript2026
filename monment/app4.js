dbaede9cdefbdfb1759404ea867deb49ca335d10
hnyou <hnyou@zaisoft.co.kr>
2026-08-03 17:48:05 +0900

    #3.6 css in javascript

--- monment/app4.js
+++ monment/app4.js
@@ -1,6 +1,46 @@
 // const hellos = document.querySelectorAll(".hello h1");
-const hellos = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
+
+console.dir(title);
+
+function handleTitleClick() {
+    console.log("title was clicked!");
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";    
+}
 
 
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
 
-console.log(hellos);
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOOOD");
+}
+
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener  ("mouseenter", handleMouseEnter);
+title.addEventListener  ("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+
+window.addEventListener("copy", handleWindowCopy)
+
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
