5f0b62f7b5faf7d10b8a68dfe1e0f84ae9c48994
hannayou1 <pooh9868@naver.com>
2026-08-05 18:09:26 +0900

    5.3 clock

---
+++ monment/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("h2#clock");
+
+clock.innerText = "00:00:00";
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000); // 1초마다 getClock 함수 실행
